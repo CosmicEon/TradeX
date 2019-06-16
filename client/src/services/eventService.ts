9edd5699e5b3901b7efe885c39fbc5dfcf49334f
Krasen Ilkov <krasen.ilkov@motion-software.com>
2019-06-16 14:15:28 +0300

    request events query

--- client/src/services/eventService.ts
+++ client/src/services/eventService.ts
@@ -1,4 +1,4 @@
-import { ObservableMap, observable, action, onBecomeObserved, computed, runInAction } from 'mobx';
+import { ObservableMap, observable, action, computed, runInAction } from 'mobx';
 import { Event } from '@models/Event';
 
 export interface GroupType {
@@ -31,8 +31,8 @@ export class EventService {
 
     this.core = core;
     this.ajax = core.getService('ajax');
-    onBecomeObserved(this, 'events', this.getAll);
-    // this.getAll();
+
+    this.getAll();
     this.getAllLeagueEvent();
     this.getAllSportEvent();
   }
@@ -53,7 +53,7 @@ export class EventService {
 
   @action
   public setSearchedLeagueEvent(searchedLeague: string) {
-    this.searchedLeagueEvent = searchedLeague;
+      this.searchedLeagueEvent = searchedLeague;
   }
 
   @action.bound
@@ -74,7 +74,7 @@ export class EventService {
 
   @action
   public setSearchedSportEvent(searchedSport: string) {
-    this.searchedSportEvent = searchedSport;
+      this.searchedSportEvent = searchedSport;
   }
 
   @action.bound
@@ -90,113 +90,31 @@ export class EventService {
         });
       })
       .catch((error) => console.log(error));
-
   }
 
   @action.bound
   public getAll() {
 
-    const mock = [
-      {
-        id: 3423432,
-        eventName: 'Sokol Kolbuszowa Dolna vs Przelom Besko',
-        eventDate: '2019-06-12T13:00:00.000Z',
-        sport: {
-          id: 1235,
-          name: 'Soccer'
-        },
-        league: {
-          id: 114666,
-          name: 'Polish 4. liga - Gr. Podkarpacka',
-          sportId: 1235
-        },
-        homeTeamScore: 0,
-        awayTeamScore: 0,
-        homeTeamOdds: 3.9,
-        awayTeamOdds: 4.2,
-        drawOdds: 1.8
-      },
-      {
-        id: 657856856345,
-        eventName: 'Wislok Wisniowa vs Izolator Boguchwala',
-        eventDate: '2019-06-12T13:00:00.000Z',
-        sport: {
-          id: 1235,
-          name: 'Soccer'
-        },
-        league: {
-          id: 114666,
-          name: 'Polish 4. liga - Gr. Podkarpacka',
-          sportId: 1235
-        },
-        homeTeamScore: 0,
-        awayTeamScore: 0,
-        homeTeamOdds: 1.5,
-        awayTeamOdds: 2,
-        drawOdds: 2.5
-      },
-      {
-        id: 8795463458232,
-        eventName: 'Wave Check vs Last Minute',
-        eventDate: '2019-06-12T18:30:00.000Z',
-        sport: {
-          id: 1298,
-          name: 'E-Sports'
-        },
-        league: {
-          id: 98470,
-          name: 'Overwatch Contenders',
-          sportId: 1298
-        },
-        homeTeamScore: 0,
-        awayTeamScore: 0,
-        homeTeamOdds: 2.6,
-        awayTeamOdds: 4.5,
-        drawOdds: 1.8
-      },
-      {
-        id: 1238758,
-        eventName: 'Reynor vs Lambo',
-        eventDate: '2019-06-12T16:30:00.000Z',
-        sport: {
-          id: 1298,
-          name: 'E-Sports'
-        },
-        league: {
-          id: 190387,
-          name: 'Starcraft 2 Bonjwa Cup',
-          sportId: 1298
-        },
-        homeTeamScore: 0,
-        awayTeamScore: 0,
-        homeTeamOdds: 4.2,
-        awayTeamOdds: 3.3,
-        drawOdds: 4.6
-      },
-    ];
+    const base: tradeX.IAjaxRequestOptions = { url: `${this.baseUrl}/api/events` };
+    if (!base.params && (this.searchedLeagueEvent !== 'all' || this.searchedSportEvent !== 'all')) {
+      base.params = {};
+    }
+    if (this.searchedLeagueEvent !== 'all') {
+      base.params.leagueId = this.searchedLeagueEvent;
+    }
+    if (this.searchedSportEvent !== 'all') {
+      base.params.sportsId = this.searchedSportEvent;
+    }
 
-    // this.ajax.get(`${this.baseUrl}/api//${}`)
-    mock.forEach(x => {
-      this.events.set(x.id, x as Event);
-      // if (this.events.has(x.id)) {
-      // }
-    });
+    this.ajax.get(base)
+      .then((response) => {
+        const convertedRes = JSON.parse(response.data);
+        runInAction(() => {
+          convertedRes.forEach(x => {
+            this.events.set(x.id, x as Event);
+          });
+        });
+      })
+      .catch((error) => console.log(error));
   }
-
-  // @action
-  // private mapEvents(dto: any) {
-
-  //   const eventDtos = dto.UpcomingEvents;
-  //   console.log(dto.UpcomingEvents);
-  //   Object.keys(dto.UpcomingEvents).map(x => {
-  //     const event = new Event();
-  //     event.id = eventDtos[x].ID;
-  //     event.type = x.substring(0, x.indexOf('Event'));
-  //     event.away = eventDtos[x].Away;
-  //     event.home = eventDtos[x].home;
-  //     event.eventTime = new Date(eventDtos[x].EventTime);
-
-  //   });
-  // }
-
 }
